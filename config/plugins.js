1da0b4bccc69c1f5fdf196fe508dba4014519949
pizzaAIdev <albertai2738@gmail.com>
2026-02-16 18:30:25 -0500

    Create plugins.js

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('CF_ACCESS_KEY_ID'),
+        secretAccessKey: env('CF_ACCESS_SECRET'),
+        region: 'auto',
+        params: {
+          Bucket: env('CF_BUCKET'),
+        },
+        endpoint: env('CF_ENDPOINT'), 
+      },
+    },
+  },
+});
