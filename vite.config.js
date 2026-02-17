2f5b0c86189efb98cf0db7dd1895e5dd3c7b5a60
pizzaAIdev <albertai2738@gmail.com>
2026-02-16 20:02:56 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-export default {
-    server: {
-        allowedHosts: true
-    }
-};
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    allowedHosts: [
+      'railwayapp-strapi-production-cd02.up.railway.app',
+      'localhost'
+    ],
+  },
+});
